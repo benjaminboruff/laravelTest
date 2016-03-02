45f7d7fa8dbc98da7d68e5c0164b0b3ffe9a86e6
Benjamin Boruff <benboruff@gmail.com>
2016-03-02 15:56:34 -0500

    contact view page

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('title')
+<title>Contact</title>
+@stop
+
+@section('content')
+
+<div class="container">
+    <div class="content">
+        <div class="title">An About page!</div>
+        <p><h5>Cool stuff, eh?</h5></p>
+    </div>
+</div>
+
+@stop
+
+@section('footer')
+
+<script>
+    alert('About page only!');
+</script>
+
+@stop
